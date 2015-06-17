a365d8d2f6968b612b16f9ceb0d3de19a09283e3
hxx0215 <hxx0215@126.com>
2015-06-17 19:53:34 +0800

    add Conn

--- zhoubao.sh
+++ zhoubao.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 defaultname=hxx0215
-echo "input username or use default name"
+workspace=~/work
+echo "input username or type enter to use default name"
 read username
-echo $username
 if [ "$username" = "" ]; then
-    echo "use default"
+    echo "you choose to use default username"
     username=$defaultname
 fi
 echo $username
 cd /Users/hxx/work/project/pocketscanner
-git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > ~/work/pocketscanner_log.txt
-sed -i '' 's/hxx0215/PocketScanner/g' ~/work/pocketscanner_log.txt
+git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/pocketscanner_log.txt
+sed -i '' "s/$username/PocketScanner/g" $workspace/pocketscanner_log.txt
+cd /Users/hxx/work/project/PDF_Connoisseur_Universal_source
+git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/Connoisseur_log.txt
+sed -i '' "s/$username/Connoisseur/g" $workspace/Connoisseur_log.txt
+cat $workspace/pocketscanner_log.txt > $workspace/zhoubao.txt
+cat $workspace/Connoisseur_log.txt >> $workspace/zhoubao.txt
+vim $workspace/zhoubao.txt
