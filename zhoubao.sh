e9ea5bcea9517ad447dcb56dc237bad49745e253
hxx0215 <hxx0215@126.com>
2015-07-10 17:58:13 +0800

    array

--- zhoubao.sh
+++ zhoubao.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 defaultname=hxx0215
 workspace=~/work
+projectPath=~/work/project
 echo "input username or type enter to use default name"
 read username
 if [ "$username" = "" ]; then
@@ -8,19 +9,19 @@ if [ "$username" = "" ]; then
     username=$defaultname
 fi
 echo $username
-cd /Users/hxx/work/project/pocketscanner
+cd $projectPath/pocketscanner
 git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/pocketscanner_log.txt
 sed -i '' "s/$username/PocketScanner/g" $workspace/pocketscanner_log.txt
-cd /Users/hxx/work/project/PDF_Connoisseur_Universal_source
+cd $projectPath/PDF_Connoisseur_Universal_source
 git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/Connoisseur_log.txt
 sed -i '' "s/$username/Connoisseur/g" $workspace/Connoisseur_log.txt
-cd /Users/hxx/work/project/PDFReader\_Universal\_source
+cd $projectPath/PDFReader\_Universal\_source
 git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/Reader6_log.txt
 sed -i '' "s/$username/PDFReader6/g" $workspace/Reader6_log.txt
-cd /Users/hxx/work/project/Pocket_Scanner_Cloud_source
+cd $projectPath/Pocket_Scanner_Cloud_source
 git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/Scanner_log.txt
 sed -i '' "s/$username/ScannerCloud/g" $workspace/Scanner_log.txt
-cd /Users/hxx/work/project/PDF_Markup_Cloud
+cd $projectPath/PDF_Markup_Cloud
 git log --pretty=format:"%an %s" --since=1.days --author=$username | grep "$username.*-" > $workspace/PDFCloud_log.txt
 sed -i '' "s/$username/PDFCloud/g" $workspace/PDFCloud_log.txt
 cat $workspace/pocketscanner_log.txt > $workspace/zhoubao.txt
